c0a17a11bea58b1895f6731ad29257c891d3eed5
ajiaz <you@example.com>
2023-05-19 10:04:48 +0530

    msg1

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int add(int a,int b)
+{
+        return a+b;
+}
+void main()
+{
+        int a=10,b=20;
+        int (*func)(int,int);
+        func = add;
+        int c = func(a,b);
+        printf("%d\n",c);
+}
+
